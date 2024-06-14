1a3942f0d73a93df0c80bf03892e2ebc9a006ee2
Hekaton003 <131018119+Hekaton003@users.noreply.github.com>
2024-06-14 15:46:05 +0200

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -1,88 +1,79 @@
-import styles from "./App.module.css";
-import slika1 from "./img.png"
-import slika2 from "./img_1.png"
-import logo from "./img_2.png"
-import {useEffect, useState} from "react";
-function Product(){
-    const [star,setStar]=useState([])
-    const [rating,setRating]=useState(0)
-    function initial(){
-        let arr = [slika1,slika1,slika1,slika1,slika1]
-        setStar(arr)
-    }
+import logo from "./img_2.png";
+import { useEffect, useState } from "react";
+import slika1 from "./img.png"; // assuming you have an empty star image
+import slika2 from "./img_1.png"; // assuming you have a filled star image
+import styles from "./App.module.css"; // assuming you have a CSS module for styles
 
+function Product() {
+    const [star, setStar] = useState([slika1,slika1,slika1,slika1,slika1]);
+    const [rating, setRating] = useState(0);
     useEffect(() => {
-
-        start();
-        if(window.closed){
-            sessionStorage.setItem("rating","0");
+        let rat = parseInt(localStorage.getItem("rating")) || 0;
+        setRating(rat);
+        console.log(rat)
+        const initialStart = Array(5).fill(slika1);
+        for (let i = 0; i < rat; i++) {
+            initialStart[i] = slika2;
         }
-
+        if(rat!=0){
+            initialStart[rat]=slika2;
+        }
+        setStar(initialStart);
     }, []);
 
-    function start(){
-         let rat = parseInt(sessionStorage.getItem("rating"))||0;
-         console.log(rat)
-         setRating(rat)
-         initial()
-         if(rating>0){
-             for (let i = 0; i < rating; i++) {
-                 NotClickStar(i);
-             }
-         }
-    }
-    function NotClickStar(index){
-        initial()
-        let arr = star;
-        if(arr[index]===slika1){
-            arr[index]=slika2
-        }
-        else {
-            arr[index]=slika1;
-        }
-        setStar(arr)
-    }
-    function changeStar(index){
-        let arr = star;
-        if(arr[index]===slika1){
+    function changeStar(index) {
+        let arr = [...star];
+        let newRating = index;
+        if (arr[index] === slika1) {
             for (let i = 0; i <= index; i++) {
-                arr[i]=slika2
+                arr[i] = slika2;
             }
-        }
-        else {
-            for(let i=index;i<5;i++){
-                arr[i]=slika1
+        } else {
+            for (let i = index; i < 5; i++) {
+                arr[i] = slika1;
             }
+            newRating=index-1;
         }
-        setStar(arr)
-        setRating(index)
-        sessionStorage.setItem("rating",rating.toString())
-        
+        setStar(arr);
+        setRating(newRating);
+        localStorage.setItem("rating", (newRating).toString());
     }
-    return(
+
+    return (
         <div className={styles.block}>
-            <p>Please rate </p>
+            <p>Please rate</p>
             <ul>
                 <li>
-                    <button onClick={()=>changeStar(0)}><img src={star[0]} alt={"star"} width={50} height={50}/></button>
+                    <button onClick={() => changeStar(0)}>
+                        <img src={star[0]} alt="star" width={50} height={50} />
+                    </button>
                 </li>
                 <li>
-                    <button key={1} onClick={()=>changeStar(1)}><img src={star[1]} alt={"star"} width={50} height={50}/></button>
+                    <button key={1} onClick={() => changeStar(1)}>
+                        <img src={star[1]} alt="star" width={50} height={50} />
+                    </button>
                 </li>
                 <li>
-                    <button key={2} onClick={()=>changeStar(2)}><img src={star[2]} alt={"star"} width={50} height={50}/></button>
+                    <button key={2} onClick={() => changeStar(2)}>
+                        <img src={star[2]} alt="star" width={50} height={50} />
+                    </button>
                 </li>
                 <li>
-                    <button key={3} onClick={()=>changeStar(3)}><img src={star[3]} alt={"star"} width={50} height={50}/></button>
+                    <button key={3} onClick={() => changeStar(3)}>
+                        <img src={star[3]} alt="star" width={50} height={50} />
+                    </button>
                 </li>
                 <li>
-                    <button key={4} onClick={()=>changeStar(4)}><img src={star[4]} alt={"star"} width={50} height={50}/></button>
+                    <button key={4} onClick={() => changeStar(4)}>
+                        <img src={star[4]} alt="star" width={50} height={50} />
+                    </button>
                 </li>
             </ul>
             <p>Your feedback is very important to us</p>
-            <img src={logo} alt={"logo"} width={164} height={52} />
+            <img src={logo} alt="logo" width={164} height={52} />
         </div>
     );
 }
 
 export default Product;
+
